9d14060610c7ad5a7bf5b91c663381d5b922ff98
Daniel Verejan <daniel.verejan@ellation.com>
2021-04-18 10:11:48 +0300

    Save lap functionality

--- index.js
+++ index.js
@@ -17,11 +17,7 @@ console.log("Here");
 
 let intervalId;
 
-let time = {
-  minutes: 0,
-  seconds: 0,
-  miliseconds: 0,
-}
+let countMiliseconds = 0;
 
 document.querySelector('.start-btn').addEventListener('click', handleStart);
 document.querySelector('.pause-btn').addEventListener('click', handlePause);
@@ -29,7 +25,7 @@ document.querySelector('.reset-btn').addEventListener('click', handleReset);
 
 function handleStart() {
   intervalId = setInterval(function() {
-    tickMiliseconds();
+    countMiliseconds++;
     showTime();
   }, 10);
 }
@@ -40,36 +36,73 @@ function handlePause() {
 
 function handleReset() {
   clearInterval(intervalId);
-  time.minutes = 0;
-  time.seconds = 0;
-  time.miliseconds = 0;
+  countMiliseconds = 0;
   showTime();
+  cleanLaps();
 }
 
-function tickMiliseconds() {
-  if (time.miliseconds === 99) {
-    time.miliseconds = 0;
-    tickSeconds();
-  } else {
-    time.miliseconds++;
-  }
-}
-
-function tickSeconds() {
-  if (time.seconds === 59) {
-    time.seconds = 0;
-    tickMinutes();
-  } else {
-    time.seconds++;
-  }
-}
-
-function tickMinutes() {
-  time.minutes++;
-}
+function convertMiliseconds(value) {
+  const minutes = Math.floor(value / 100 / 60) % 60;
+  const seconds = Math.floor(value / 100) % 60;
+  const miliseconds = value % 100;
+  
+  return { minutes, seconds, miliseconds };
+};
 
 function showTime() {
-  document.querySelector('.minutes').innerText = time.minutes < 10 ? '0' + time.minutes + 'm' : time.minutes + 'm';
-  document.querySelector('.seconds').innerText = time.seconds < 10 ? '0' + time.seconds + 's' : time.seconds + 's';
-  document.querySelector('.miliseconds').innerText = time.miliseconds < 10 ? '0' + time.miliseconds + 'ms' : time.miliseconds + 'ms';
+  const {minutes, seconds, miliseconds} = convertMiliseconds(countMiliseconds);
+
+  document.querySelector('.minutes').innerText = `${minutes < 10 ? '0' + minutes : minutes}m`;
+  document.querySelector('.seconds').innerText = `${seconds < 10 ? '0' + seconds : seconds}s`;
+  document.querySelector('.miliseconds').innerText = `${miliseconds < 10 ? '0' + miliseconds : miliseconds}ms`;
+}
+
+
+// Bonus, lap counter
+document.querySelector('.lap-btn').addEventListener('click', handleLap);
+let lapCounter = 0;
+let lapTimeMS = countMiliseconds;
+
+function getLapItemTimeHTML(time) {
+  const {minutes, seconds, miliseconds} = convertMiliseconds(time);
+  
+  const div = document.createElement('div');
+  div.classList.add('laps__item-time');
+
+  div.innerHTML = `
+    <span>${minutes < 10 ? '0' + minutes : minutes}m</span>
+    <span>:</span>
+    <span>${seconds < 10 ? '0' + seconds : seconds}s</span>
+    <span>:</span>
+    <span>${miliseconds < 10 ? '0' + miliseconds : miliseconds}ms</span>
+  `;
+
+  return div;
+}
+
+function getLapItemHTML() {
+  const li = document.createElement('li');
+  li.classList.add('laps__item');
+
+  const h3 = document.createElement('h3');
+  h3.classList.add('laps__item-number');
+  h3.innerText = '#' + lapCounter;
+
+  li.append(h3);
+
+  li.append(getLapItemTimeHTML(countMiliseconds - lapTimeMS));
+  li.append(getLapItemTimeHTML(countMiliseconds));
+
+  return li;
+}
+
+function handleLap() {
+  document.querySelector('.laps__list').prepend(getLapItemHTML());
+  lapTimeMS = countMiliseconds;
+  lapCounter++;
+}
+
+function cleanLaps() {
+  document.querySelector('.laps__list').innerHTML = '';
+  lapCounter = 0;
 }
